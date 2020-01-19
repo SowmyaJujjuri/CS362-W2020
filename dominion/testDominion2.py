b31aafd3d0f193d6c905666f6b80618b8a944304
Sowmya Jujjuri <jujjuris@flip2.engr.oregonstate.edu>
2020-01-19 14:05:14 -0800

    need to make locally

---
+++ dominion/testDominion2.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 13:22:25 2020
+
+@author: Sowmya Jujjuri
+"""
+import Dominion
+import random
+import testUtility
+from collections import defaultdict
+
+#Get player names
+player_names = ["Annie","*Ben","*Carla"]
+
+#number of curses and victory cards
+if len(player_names)>2:
+    nV=12
+else:
+    nV=8
+nC = -10 + 10 * len(player_names)
+
+box = testUtility.getBoxes(nV)
+
+supply_order = testUtility.getSupplyOrder()
+
+supply = testUtility.getSupply(nV, player_names, box, nC)
+
+#initialize the trash
+trash = []
+
+players = testUtility.constructPlayer(player_names)
+
+testUtility.playGame(supply, supply_order, players, trash)
+            
+
+testUtility.finalScore(players)
